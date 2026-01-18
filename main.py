5463a3bcf3e91ca6aaf52c6c4d9747ca92849806
Steven Truong <s.ttruong@yahoo.com>
2026-01-17 19:10:31 -0500

    updating images

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.graphics import Rectangle
+from kivy.graphics import Rectangle, Color
 from kivy.properties import ObjectProperty
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
+from kivy.core.image import Image as CoreImage
 import random
+
 
 class Dice(Widget):
     img_rect = ObjectProperty(None)
@@ -13,9 +15,29 @@ class Dice(Widget):
         super().__init__(**kwargs)
 
         # Select a random image and set it as the source of the Rectangle instruction
-        img_source = self.images()
-        self.img_rect = Rectangle(source=img_source, pos=self.pos, size=self.size, aspect_ratio=None)
+        self.img_source = self.images()
+        self.img_rect = Rectangle(source=self.img_source, pos=self.pos, size=self.size, aspect_ratio=None)
         self.canvas.add(self.img_rect)
+
+        self.bind(pos=self.update_rect, size=self.update_rect)
+
+    def update_rect(self, *args):
+        # Load original image size
+        img = CoreImage(self.img_source)
+        iw, ih = img.size
+        ww, wh = self.size
+
+        # Keep aspect ratio
+        scale = min(ww / iw, wh / ih)
+        new_w = iw * scale
+        new_h = ih * scale
+
+        # Center the image
+        x = self.x + (ww - new_w) / 2
+        y = self.y + (wh - new_h) / 2
+
+        self.img_rect.pos = (x, y)
+        self.img_rect.size = (new_w, new_h)
 
     def on_size(self, *args):
         # Update the size of the Rectangle instruction when the widget is resized
@@ -26,17 +48,29 @@ class Dice(Widget):
         self.img_rect.pos = self.pos
 
     def images(self):
-        rolls = ["./images/ca.png", "./images/tom.png", "./images/cua.png", "./images/ga.png", "./images/bau.png", "./images/deer.png"]
+        rolls = ["./images/caDice.png", "./images/tomDice.png", "./images/cuaDice.png", "./images/gaDice.png", "./images/bauDice.png", "./images/deerDice.png"]
         return random.choice(rolls)
     
     def update_image(self):
         #update with new random image
-        img_source = self.images()
-        self.img_rect.source = img_source
+        self.img_source = self.images()
+        self.img_rect.source = self.img_source
+        self.update_rect()
     
 class DiceApp(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
+        # Draw a white background behind everything
+        # with self.canvas.before:
+        #     Color(1, 1, 1, 1)  # white
+        #     self.bg = Rectangle(pos=self.pos, size=self.size)
+        # self.bind(pos=self.update_bg, size=self.update_bg)
+
+        # Add background image
+        with self.canvas.before:
+            self.bg_image = Rectangle(source="./images/background.jpg", pos=self.pos, size=self.size)
+        self.bind(pos=self.update_bg, size=self.update_bg)
 
         #Create a dice widget and a button widget
         self.dice = Dice()
@@ -53,7 +87,24 @@ class DiceApp(BoxLayout):
         self.add_widget(self.dice2)
         self.add_widget(self.dice3)
         self.add_widget(self.button)
+    
+    def update_bg(self, *args):
+        # Keep background scaled to window
+        img = CoreImage("./images/background.jpg")
+        iw, ih = img.size
+        ww, wh = self.size
 
+        # Keep aspect ratio
+        scale = min(ww / iw, wh / ih)
+        new_w = iw * scale
+        new_h = ih * scale
+
+        # Center background
+        x = self.x + (ww - new_w) / 2
+        y = self.y + (wh - new_h) / 2
+
+        self.bg_image.pos = (x, y)
+        self.bg_image.size = (new_w, new_h)
 
 class BauCua(App):
     def build(self):
